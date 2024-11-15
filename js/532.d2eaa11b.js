0a51786647e544531fb5157b12cf32095f5b1003
JF29771_kbcgroup <boris.stankovv@gmail.com>
2024-11-15 17:05:51 +0200

    changed the content

--- js/532.d2eaa11b.js
+++ js/532.d2eaa11b.js
@@ -269,7 +269,7 @@
     //screensaver video
     1119: function (e, t, i) {
       "use strict";
-      e.exports = i.p + "media/24321_HOLOPOD_SCREENSAVER_Holopod_2160x3840_5000.mp4";
+      e.exports = i.p + "media/NEW_HOLOPOD_SCREENSAVER_Holopod_1_2160x3840_5000.mp4";
     },
   },
 ]);
